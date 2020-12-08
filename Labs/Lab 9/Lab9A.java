a54893ee7a2d79eee926e3b53dfeeeec1e9c427f
TeeGee3 <63059919+TeeGee3@users.noreply.github.com>
2020-12-07 21:34:57 -0500

    Add files via upload

---
+++ Labs/Lab 9/Lab9A.java
@@ -0,0 +1,18 @@
+/*
+Class: CSE 1321L
+Section: W01
+Term: Fall 2020
+Instructor: Keerthi Manchukonda
+Name: Taylor Goff
+Assignment 5
+
+*/
+
+import java.util.*;
+
+public class Lab9A {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
